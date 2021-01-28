507f087337fb6c306086ac43d365c88166926a8a
Corbetta Marco <corbetta.marco@osdbsesto>
2021-01-28 13:42:20 +0100

    provaNode quasi finita

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var http = require("http");
 var express = require("express");
+const { dirname } = require("path");
 var apiServer = express();
 
 var hostname = "127.0.0.1";
 var port = 3000;
+var nome = "marco"
+var path = require ("path");
 
 apiServer.listen(port, hostname, () => {
     console.log("server running at http://%s:%d/", hostname, port);
@@ -11,10 +14,20 @@ apiServer.listen(port, hostname, () => {
 
 apiServer.get("/nome", (req, res) =>{
     console.log ("ho ricevuto una get su nome");
-    res.send ("ciao, il nome del server è: node")
+    res.send ("ciao, il nome del server è: "+ nome);
 });
 
 
 apiServer.get("/", function(req, res){
     res.send("sei in home");
 })
+
+apiServer.get("/index", function(req, res){
+    //res.sendFile("C:\\Users\\corbetta.marco\\Desktop\\prova node\\provaNode\\index.html");
+    res.sendFile(path.join(__dirname + "\\index.html"));
+})
+
+apiServer.get ("/mioNome", (req, res)=>{
+    console.log ("ho ricevuto una get su nome con query", req.query);
+    res.send ("ciao, il nome del server è: "+ req.query.nome);
+});
